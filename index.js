b173cd8ed1867cf7b70df7f5b81a8a91b03d55ea
HiAaronCody <aaron.cody@gmail.com>
2023-09-14 08:27:30 -0400

    export base chmln object

--- index.js
+++ index.js
@@ -1,54 +1,32 @@
-const init = (token, { fastUrl } = {}) => {
-  if (typeof chmln !== 'undefined') {
-    console.log(`The global variable \`chmln\` is already defined.\nPlease make sure you're not adding Chameleon to the page other than with this library.`);
-    return;
-  }
 
-  var d = document;
-  var w = window;
-  var t = token,
-  c="chmln",m="identify alias track clear set show on off custom help _data".split(" "),
-  i=d.createElement("script");if(w[c]||(w[c]={}),!w[c].root){w[c].accountToken=t,w[c].location=w.location.href.toString(),w[c].fastUrl=fastUrl||'https://fast.trychameleon.com/',
-      w[c].now=new Date;for(var s=0;s<m.length;s++){!function(){var t=w[c][m[s]+"_a"]=[];w[c][m[s]]=function(){t.push(arguments);};}();}
-      i.src=w[c].fastUrl+"messo/"+t+"/messo.min.js",
-      i.async=!0,d.head.appendChild(i);}
-}
+const noop_available = () => { console.log(`Failed to run because the chameleon.io script was not initialized yet.`);
+return; };
 
-const identify = (id, options) => {
-  if (!window.chmln || !window.chmln.identify) {
-    console.log(`Failed to identify the user [${id}] because the chameleon.io script was not initialized yet.`);
-    return;
-  }
-
-  window.chmln.identify(id, options || {});
-}
-
-const track = api_factory('track');
-const clear = api_factory('clear');
-const set = api_factory('set');
-const show = api_factory('set');
-const on = api_factory('on');
-const off = api_factory('off');
-
-const api_factory = (method_name) => {
-  return function() {
-    if (!chmln || !chmln[method_name]) {
-      console.log(`Failed to call [${method_name}] because the chameleon.io script was not initialized yet.`);
+const chmln_object = {
+  init: (token, { fastUrl } = {}) => {
+    if (typeof chmln !== 'undefined') {
+      console.log(`The global variable \`chmln\` is already defined.\nPlease make sure you're not adding Chameleon to the page other than with this library.`);
       return;
     }
-
-    window.chmln[method_name].apply(this, arguments);
-  }
-}
-
-
-module.exports = {
-  init,
-  identify,
-  track,
-  clear,
-  set,
-  show,
-  on,
-  off
+  
+    var d = document;
+    var w = window;
+    var t = token,
+    c="chmln",m="identify alias track clear set show on off custom help _data".split(" "),
+    i=d.createElement("script");
+    if(w[c]||(w[c]=chmln_object),!w[c].root){w[c].accountToken=t,w[c].location=w.location.href.toString(),w[c].fastUrl=fastUrl||'https://fast.trychameleon.com/',
+        w[c].now=new Date;for(var s=0;s<m.length;s++){!function(){var t=w[c][m[s]+"_a"]=[];w[c][m[s]]=function(){t.push(arguments);};}();}
+        i.src=w[c].fastUrl+"messo/"+t+"/messo.min.js",
+        i.async=!0,d.head.appendChild(i),w[c].init=null;}
+  },
+  identify: noop_available,
+  alias: noop_available,
+  track: noop_available,
+  clear: noop_available,
+  set: noop_available,
+  show: noop_available,
+  on: noop_available,
+  off: noop_available
 };
+
+module.exports = chmln_object;
