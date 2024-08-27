e91b2504bd7004a012031c5bf29f6bd5442fb8b0
HiAaronCody <aaron.cody@gmail.com>
2024-08-27 11:39:01 -0400

    further code cleanup and formatting

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 
 let chmln_object = window.chmln;
 
-if(typeof chmln_object === 'undefined' || !chmln_object.identify) {
+if (typeof chmln_object === 'undefined' || !chmln_object.identify) {
   chmln_object = chmln_object || (window.chmln = {});
   const defaultFunctions = "identify alias track clear set show on off custom help _data".split(" ");
-  for(var s=0;s<defaultFunctions.length;s++){
-    !function(){
-      const t = chmln_object[defaultFunctions[s]+"_a"] = [];
-      chmln_object[defaultFunctions[s]] = function() { t.push(arguments); };
-    }();
-  };
+  for (var i = 0; i < defaultFunctions.length; i++) {
+    (!function() {
+      const placeholder_function = chmln_object[defaultFunctions[i]+"_a"] = [];
+      chmln_object[defaultFunctions[i]] = function() { placeholder_function.push(arguments); };
+    }());
+  }
 
   chmln_object.init = (token, { fastUrl } = {}) => {
-    var d = document;
-    var w = window;
-    var t = token;
-    var i = d.createElement("script");
+    const d = document;
+    const w = window;
+    const t = token;
+    const s = d.createElement("script");
     chmln_object.accountToken = t;
     chmln_object.location = w.location.href.toString();
     chmln_object.now = new Date;
     chmln_object.fastUrl = fastUrl || 'https://fast.chameleon.io/';
-    i.src = chmln_object.fastUrl+"messo/"+t+"/messo.min.js";
-    i.async = true;
-    d.head.appendChild(i);
-  }
+    s.src = chmln_object.fastUrl + "messo/" + t + "/messo.min.js";
+    s.async = true;
+    d.head.appendChild(s);
+  };
 }
 
 module.exports = chmln_object;
